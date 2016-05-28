f49839d519ae59f1b083f21f130bcadb419f605d
Julian Laubstein <julianlaubstein@yahoo.de>
2016-05-28 13:01:15 +0200

    Enhanced nickel example

--- examples/frameworks/nickel/src/main.rs
+++ examples/frameworks/nickel/src/main.rs
@@ -1,9 +1,18 @@
 #[macro_use] extern crate nickel;
 
-use nickel::{Nickel, HttpRouter};
+use nickel::{Nickel, HttpRouter, StaticFilesHandler};
 
 fn main() {
-  let mut server = Nickel::new();
-  server.get("/", middleware!("Hello world!"));
-  server.listen("127.0.0.1:3000");
+    let mut server = Nickel::new();
+
+    server.utilize(StaticFilesHandler::new("public/"));
+
+    server.get("/bar", middleware!("This is the /bar handler"));
+    server.get("/user/:userid", middleware! { |request|
+        format!("This is user: {:?}", request.param("userid"))
+    });
+    server.get("/a/*/d", middleware!("matches /a/b/d but not /a/b/c/d"));
+    server.get("/a/**/d", middleware!("This matches /a/b/d and also /a/b/c/d"));
+
+    server.listen("127.0.0.1:3000");
 }
